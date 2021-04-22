4ad117aab8242829f26677e9cb8e45e724015685
Kirillov Alexander <kirillovbrest@gmail.com>
2021-04-22 16:06:32 +0300

    Remove flag implemented

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class PostRepository {
@@ -16,16 +17,19 @@ public class PostRepository {
   private AtomicInteger index = new AtomicInteger(0);
 
   public List<Post> all() {
-    return postList;
+    return postList.stream()
+            .filter(post -> !(post.isRemoved()))
+            .collect(Collectors.toList());
   }
 
   public Optional<Post> getById(long id) {
     return postList.stream()
-            .filter(o -> o.getId() == id)
+            .filter(post -> (!post.isRemoved()))
+            .filter(post -> post.getId() == id)
             .findFirst();
   }
 
-  public Post save(Post post) {
+  public Post save(Post post) throws NotFoundException {
     if (post.getId() == 0) {
       post.setId(index.incrementAndGet());
       postList.add(post);
@@ -33,7 +37,7 @@ public class PostRepository {
     } else {
       for (int i = 0; i < postList.size(); i++) {
         Post currentPost = postList.get(i);
-        if (currentPost.getId() == post.getId()) {
+        if (currentPost.getId() == post.getId() && !currentPost.isRemoved()) {
           currentPost.setContent(post.getContent());
           return currentPost;
         }
@@ -43,10 +47,8 @@ public class PostRepository {
   }
 
   public void removeById(long id) {
-    int index = -1;
     for (int i = 0; i < postList.size(); i++) {
-      if (postList.get(i).getId() == id) index = i;
+      if (postList.get(i).getId() == id) postList.get(i).setRemoved(true);
     }
-    if (index != -1) postList.remove(index);
   }
 }
